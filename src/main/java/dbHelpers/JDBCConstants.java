bf2cb6ad021ee06644b562c19dc9008578342e5c
alistairzhu <alistairzhu@gmail.com>
2018-12-01 12:52:43 -0500

    add cdbHelpers and one implementation

---
+++ src/main/java/dbHelpers/JDBCConstants.java
@@ -0,0 +1,11 @@
+package dbHelpers;
+
+public class JDBCConstants
+{
+
+    public static final String DRIVER_NAME = "org.postgresql.Driver";
+    public static final String URL         = "jdbc:postgresql://127.0.0.1:5432/postgres";
+    public static final String USERNAME    = "postgres";
+    public static final String PASSWORD    = "1199";
+
+}
---
+++ src/main/java/dbHelpers/SQLQueries.java
@@ -0,0 +1,22 @@
+package dbHelpers;
+
+public class SQLQueries {
+
+
+    /**
+     * Table name : PERSON
+     *
+     * SQL:
+     * create table PERSON (ID bigint not null, EMAIL varchar(255), FIRST_NAME varchar(255), JOINED_DATE date, LAST_NAME varchar(255), primary key (id))
+     *
+     */
+    public static final String INSERT_SQL_QUERY     = "INSERT INTO PERSON(ID,FIRST_NAME,LAST_NAME,EMAIL,JOINED_DATE) VALUES(?,?,?,?,?)";
+    //public static final String INSERT_SQL_QUERY     = "INSERT INTO public.acccounts(accoutName, addressSet) VALUES (?, ?)";
+    public static final String UPDATE_SQL_QUERY     = "UPDATE PERSON SET FIRST_NAME=? WHERE ID=?";
+    public static final String SELECT_SQL_QUERY     = "SELECT ID,FIRST_NAME,LAST_NAME,EMAIL,JOINED_DATE FROM PERSON WHERE ID=?";
+    public static final String SELECT_ALL_SQL_QUERY = "SELECT ID,FIRST_NAME,LAST_NAME,EMAIL,JOINED_DATE FROM PERSON";
+    public static final String DELETE_SQL_QUERY     = "DELETE FROM PERSON WHERE ID=?";
+    public static final String DELETE_ALL_SQL_QUERY = "DELETE FROM PERSON";
+    public static final String SELECT_PERSON_QUERY  = "SELECT * FROM PERSON";
+
+}
