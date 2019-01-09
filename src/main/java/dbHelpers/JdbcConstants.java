02bb7e1c445a0905dde8cc7c889b6bd886f0acef
alistairzhu <alistairzhu@gmail.com>
2019-01-08 20:36:07 -0800

    Update JdbcConstants.java

--- src/main/java/dbHelpers/JdbcConstants.java
+++ src/main/java/dbHelpers/JdbcConstants.java
@@ -6,6 +6,6 @@ public class JdbcConstants
     public static final String DRIVER_NAME = "org.postgresql.Driver";
     public static final String URL         = "jdbc:postgresql://127.0.0.1:5432/postgres";
     public static final String USERNAME    = "postgres";
-    public static final String PASSWORD    = "1199";
+    public static final String PASSWORD    = "";
 
 }
